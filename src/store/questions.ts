ba6895947e737069179d96f7cecd3573a10283cd
Joa Pozzo <pozzojoa@gmail.com>
2024-12-04 11:35:44 -0300

    se añadio persistencia y correcciones en rutas de produccion

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { Question } from "../types/types";
 import confetti from "canvas-confetti";
 
@@ -13,71 +14,71 @@ interface QuestionsState {
     reset: () => void;
 }
 
-const API_URL = "http://localhost:5173";
+const API_URL = import.meta.env.PROD ? 'https://quizz-gp.vercel.app/' : 'http://localhost:5173/';
 
-export const useQuestionsStore = create<QuestionsState>((set, get) => ({
-    loading: false,
-    questions: [],
-    currentQuestion: 0,
-
-    fetchQuestions: async (limit: number) => {
-        try {
-            set({ loading: true });
-            const res = await fetch(`${API_URL}/data.json`);
-            const data = await res.json();
-
-            const questions = data.sort(() => Math.random() - 0.5).slice(0, limit);
-            set({ questions, loading: false });
-        } catch (error) {
-            console.error("Error fetching questions:", error);
-            set({ loading: false });
-        }
-    },
-
-    selectAnswer: (questionId: number, answerId: number) => {
-        // Recupermos las preguntas del estado
-        const { questions } = get();
-        // Clonamos las preguntas para no modificar el estado
-        const newQuestions = structuredClone(questions);
-        // Obtenemos el incide de la pregunta seleccionada
-        const questionIndex = newQuestions.findIndex((question) => question.id === questionId);
-        // Obtenemos las respuestas de la pregunta seleccionada
-        const questionInfo = newQuestions[questionIndex]
-        // Verificamos si la respuesta es correcta
-        const isCorrectUserAnswer = questionInfo.correctAnswer === answerId;
-
-        if (isCorrectUserAnswer) confetti();
-
-        newQuestions[questionIndex] = {
-            ...questionInfo,
-            isCorrectUserAnswer,
-            userSelectedAnswer: answerId,
-        }
-
-        set({ questions: newQuestions })
-    },
-
-    goNextQuestions: () => {
-        const { questions, currentQuestion } = get();
-        const nextQuestion = currentQuestion + 1;
-        if (nextQuestion < questions.length) {
-            set({ currentQuestion: nextQuestion });
-        }
-    },
-
-    goPreviousQuestions: () => {
-        const { currentQuestion } = get();
-        const previosQuestion = currentQuestion - 1;
-        if (previosQuestion >= 0) {
-            set({ currentQuestion: previosQuestion });
-        }
-    },
-
-    reset: () => {
-        set({
+export const useQuestionsStore = create(
+    persist<QuestionsState>(
+        (set, get) => ({
+            loading: false,
             questions: [],
             currentQuestion: 0,
-            loading: false,
-        });
-    },
-}));
+
+            fetchQuestions: async (limit: number) => {
+                try {
+                    set({ loading: true });
+                    const res = await fetch(`${API_URL}/data.json`);
+                    const data = await res.json();
+
+                    const questions = data.sort(() => Math.random() - 0.5).slice(0, limit);
+                    set({ questions, loading: false });
+                } catch (error) {
+                    console.error("Error fetching questions:", error);
+                    set({ loading: false });
+                }
+            },
+
+            selectAnswer: (questionId: number, answerId: number) => {
+                const { questions } = get();
+                const newQuestions = structuredClone(questions);
+                const questionIndex = newQuestions.findIndex((q) => q.id === questionId);
+                const questionInfo = newQuestions[questionIndex];
+
+                const isCorrectUserAnswer = questionInfo.correctAnswer === answerId;
+                if (isCorrectUserAnswer) confetti();
+
+                newQuestions[questionIndex] = {
+                    ...questionInfo,
+                    isCorrectUserAnswer,
+                    userSelectedAnswer: answerId,
+                };
+
+                set({ questions: newQuestions });
+            },
+
+            goNextQuestions: () => {
+                const { currentQuestion, questions } = get();
+                if (currentQuestion + 1 < questions.length) {
+                    set({ currentQuestion: currentQuestion + 1 });
+                }
+            },
+
+            goPreviousQuestions: () => {
+                const { currentQuestion } = get();
+                if (currentQuestion > 0) {
+                    set({ currentQuestion: currentQuestion - 1 });
+                }
+            },
+
+            reset: () => {
+                set({
+                    questions: [],
+                    currentQuestion: 0,
+                    loading: false,
+                });
+            },
+        }),
+        {
+            name: "questions"
+        }
+    )
+);
